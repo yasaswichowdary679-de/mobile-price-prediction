e24a67d8c8edcd44c02dda50f7d8c217288a0a37
yasaswichowdary679-de <yasaswichowdary679@gmail.com>
2025-08-15 13:27:57 +0530

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,26 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+        maven("https://androidx.dev/storage/compose-compiler/repository/")
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://androidx.dev/storage/compose-compiler/repository/")
+    }
+}
+
+rootProject.name = "project"
+include(":app")
+ 
